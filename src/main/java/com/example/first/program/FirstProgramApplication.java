71f699243110863d69e55536f75c8af397711f10
umeshams28 <umesha@revolusyssolutions.com>
2023-07-11 15:03:43 +0530

    intiale commit

---
+++ src/main/java/com/example/first/program/FirstProgramApplication.java
@@ -0,0 +1,16 @@
+package com.example.first.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProgramApplication.class, args);
+	
+	
+	
+	}
+
+}
